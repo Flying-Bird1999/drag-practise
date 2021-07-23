b100e311891877f2dfee7089fa4399a538d5f811
Flying-Bird1999 <1424766809@qq.com>
2021-07-23 10:11:00 +0800

    修复bug

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,10 +20,10 @@
         <div class="center">
             <div class="view-content" @dragover="dragOver" @drop="drop">
                 <Draggable v-model="view" draggable=".item" tag="ul">
-                <!--<ul>-->
+                    <!--<ul>-->
                     <li v-for="(item, i) in view"
-                        :key="i"
-                        @click="showEdit(i)"
+                        :key="item.id"
+                        @click="showEdit(item.id)"
                         class="item" >
                         <div class="type" v-if="item.flag===1">{{item.type}}</div>
                         <component
@@ -31,17 +31,17 @@
                                 :editData="item.data"
                                 v-else
                         ></component>
-                        <i @click="deleteItem(i)" class="el-icon-error"></i>
+                        <i @click="deleteItem(item.id)" class="el-icon-error"></i>
                     </li>
-                <!--</ul> -->
+                    <!--</ul> -->
                 </Draggable>
             </div>
         </div>
         <div class="right">
             <Edit
-                :data="props"
-                v-if="rightIsShow"
-                @dataChange="getData"
+                    :data="props"
+                    v-if="rightIsShow"
+                    @dataChange="getData"
             />
         </div>
     </div>
@@ -76,12 +76,14 @@
                 view: [], //center中用于展示组件的数据
                 type: '', //拖拽元素的类型
                 isPush: false, //判断拖拽元素是否已添加到页面
-                i: -1,
+                i: -1, //临时变量，拖拽时记录组件的id值，方便释放时删除flag属性
 
                 props: {}, //被点击组件的所有信息，用于传给‘修改组件属性值的组件’
                 rightIsShow: false,  //用于控制右边栏的显示与隐藏
 
-                index: -1, //页面正在修改的值在数组里的索引
+                index: -1, //临时记录页面正在修改的组件的id值
+
+                id: 0 //给每一个组件定义一个唯一索引，每次递增
             }
         },
         methods: {
@@ -95,13 +97,15 @@
                     return
                 }
                 e.preventDefault()
-                let defaultData = {
-                    type: this.type,  // 组件类型
-                    data: {},  // 组件数据，默认为空
-                    flag: 1
-                }
                 if(!this.isPush) {  //判断如果还没添加进去的话...
-                    this.i = this.view.length
+                    let defaultData = {
+                        id: this.id++,  // 给每一个组件定义一个唯一索引
+                        type: this.type,  // 组件类型
+                        data: {},  // 组件数据，默认为空
+                        flag: 1 ////临时变量，拖拽时给组件附上该属性，释放时删除该属性
+                    }
+                    this.i = defaultData.id  //方便释放时找到flag属性，并删除
+                    // console.log(this.i)
                     this.view.push(defaultData)
                     this.isPush = true  //表示元素已添加
                 }
@@ -111,16 +115,19 @@
                     return
                 }
                 e.preventDefault()
+                // 删除组件的flag属性
+                this.$delete(this.view.filter(item=>item.id===this.i)[0], 'flag')
+                // this.$delete(this.view[this.i], 'flag')
                 // 拖拽结束，isPush和type都置为初始值
-                this.$delete(this.view[this.i], 'flag')
                 this.isPush = false
                 this.type = null
-                console.log(this.view)
+                // console.log(this.view)
             },
-            showEdit(index){
-                this.index = index  //点击组件，记录当前组件在数组中的索引
+            showEdit(id){
+                this.index = id  //点击组件，记录当前点击组件的id值
                 this.rightIsShow = false
-                this.props = this.view[index] //将该组件的值赋给props，传给子组件
+                //将该组件的值赋给props，传给子组件
+                this.props = this.view.filter(item=>item.id===id)[0]
                 // console.log(this.props)
                 this.$nextTick(()=>{
                     this.rightIsShow = true //在dom更新后改变rightIsShow，可刷新页面
@@ -128,11 +135,11 @@
             },
             getData(data){
                 // 接受孙组件，例如：ButtonEdit.vue 中的组件data值，并根据索引index赋给view中对应的data属性，可以传给子组件Button.vue
-                this.view[this.index].data = data
-                console.log(this.view)
+                this.view.filter(item=>item.id===this.index)[0].data = data
+                // console.log(this.view)
             },
-            deleteItem(i){
-                this.view.splice(i, 1)
+            deleteItem(id){
+                this.view = this.view.filter(item => item.id !== id)
                 this.rightIsShow = false
                 this.props = {}
             }
